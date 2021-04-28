ff28665787d480b4e259f294aef00858d21e36b7
Sam Queen <samqueenbusiness@gmail.com>
2021-04-28 15:32:30 -0700

    Added iphone browser webkits

--- animations.js
+++ animations.js
@@ -3,17 +3,8 @@ $(window).on('scroll', function() {
     let scrollTop = $(document).scrollTop();
     
     if(scrollTop > 0){
-        $('nav').addClass('style');
+        $('nav').addClass('shrink');
     } else {
-        $('nav').removeClass('style');
+        $('nav').removeClass('shrink');
     }
 });
-
-//mobile dropdown menu
-$('.mobile-icon').on('click', function() {
-    $('nav').toggleClass('drop-down');
-    $('nav h1').toggleClass('move-top');
-    $('nav .mobile-icon').toggleClass('move-top');
-    $('nav ul').toggleClass('show-mobile-nav');
-});
-
