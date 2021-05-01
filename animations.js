1a874831bc7ed08d45dcde3323f1d09319142557
Sam Queen <samqueenbusiness@gmail.com>
2021-05-01 16:33:23 -0700

    added logo

--- animations.js
+++ animations.js
@@ -4,7 +4,22 @@ $(window).on('scroll', function() {
     
     if(scrollTop > 0){
         $('nav').addClass('shrink');
+        $('nav img').addClass('change-height');
     } else {
         $('nav').removeClass('shrink');
+        $('nav img').removeClass('change-height');
     }
+});
+
+//drop down responsive menu
+$('nav i').on('click', function() {
+    //increase the hight of the navbar when menu icon is clicked
+    $('.mobile-ul').toggleClass('nav-grow');
+    $('nav ul').toggleClass('show');
+});
+
+//close mobile dropdown if link is clicked
+$('nav a').on('click', function() {
+    $('.mobile-ul').toggleClass('nav-grow');
+    $('nav ul').toggleClass('show');
 });
