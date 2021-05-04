15d9daa275bd98495836474392346a151f5082a6
Sam Queen <samqueenbusiness@gmail.com>
2021-05-04 16:02:04 -0700

    Added email section

--- animations.js
+++ animations.js
@@ -23,3 +23,8 @@ $('nav a').on('click', function() {
     $('.mobile-ul').toggleClass('nav-grow');
     $('nav ul').toggleClass('show');
 });
+
+//call when click phone icon in contact section
+$('.phone-icon').on('click', function() {
+   window.open('tel:4802499898'); 
+});
