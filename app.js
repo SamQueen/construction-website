9a27824e3997540a0812c1c5de1adc76e67a7a03
Sam Queen <samqueenbusiness@gmail.com>
2021-05-05 16:16:50 -0700

    added server side

--- app.js
+++ app.js
@@ -1,11 +1,60 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
+
+const nodemailer = require('nodemailer');
+
+//send email
+function sendMail(email) {
+    
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'homeremodeling.ae@gmail.com',
+            pass:  process.env.EMAIL_PASSWORD
+        }
+    });
+
+    var mailOptions = {
+        from: 'homeremodeling.ae@gmail.com',
+        to: 'homeremodeling.ae@gmail.com',
+        subject: 'Customer Email From Website',
+        text: 'Name: ' + email.userName + '\n\nEmail: ' + email.userEmail + '\n\nMessage: ' + email.msg
+    };
+
+    transporter.sendMail(mailOptions, function(error, info) {
+        if(error) {
+            console.log('Error sending email');
+            console.log(error);
+        } else {
+            console.log('Email sent!');
+        }
+    });
+};
 
 app.use(express.static("public"));
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', function(req, res) {
+    //get data from form
+    let email = {
+        userName: req.body.user_name,
+        userEmail: req.body.user_email,
+        msg: req.body.user_msg
+    };
+    
+    //send email
+    sendMail(email);
 });
 
 app.listen(port, function() {
